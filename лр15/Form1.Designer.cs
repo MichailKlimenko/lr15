5699c43f841ac1e7338ebd08aff4d3b60b7d688f
WolfGGO <klimenkomisa1@gmail.com>
2024-04-25 09:44:17 +0300

    Добавьте файлы проекта.

---
+++ лр15/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace лр15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(179, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 192);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 300);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ЛР15";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ лр15/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр15
+{
+    public partial class Form1 : Form
+    {
+        // Список для хранения заметок
+        private List<Note> notes = new List<Note>();
+        // Путь к файлу для сохранения заметок
+        private string filePath = "notes.json";
+
+        // Конструктор формы
+        public Form1()
+        {
+            InitializeComponent();
+            // Привязка обработчика события выбора даты в календаре
+            monthCalendar1.DateSelected += monthCalendar1_DateSelected;
+            // Привязка обработчика события нажатия на кнопку сохранения заметки
+            button2.Click += button2_Click;
+            // Привязка обработчика события нажатия на кнопку удаления заметки
+            button1.Click += button1_Click;
+
+            // Загрузка заметок при запуске приложения
+            LoadNotes();
+        }
+
+        // Обработчик нажатия на кнопку удаления заметки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получение выбранной даты
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            // Поиск заметки по выбранной дате
+            Note note = FindNoteByDate(selectedDate);
+            // Если заметка найдена
+            if (note != null)
+            {
+                // Удаление заметки из списка
+                notes.Remove(note);
+                // Сохранение обновленного списка заметок
+                SaveNotes();
+                // Очистка текстового поля
+                textBox1.Text = "";
+                // Изменение текста кнопки на "Сохранить"
+                button2.Text = "Сохранить";
+                // Отключение кнопки удаления заметки
+                button1.Enabled = false;
+                // Вывод сообщения об успешном удалении заметки
+                MessageBox.Show("Заметка удалена", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Обработчик нажатия на кнопку сохранения заметки
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Получение выбранной даты
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            // Получение текста заметки
+            string noteText = textBox1.Text.Trim();
+
+            // Проверка наличия текста в заметке
+            if (!string.IsNullOrEmpty(noteText))
+            {
+                // Поиск существующей заметки по выбранной дате
+                Note existingNote = FindNoteByDate(selectedDate);
+                // Если заметка существует
+                if (existingNote != null)
+                {
+                    // Обновление текста существующей заметки
+                    existingNote.Text = noteText;
+                }
+                else
+                {
+                    // Добавление новой заметки в список
+                    notes.Add(new Note { Date = selectedDate, Text = noteText });
+                }
+                // Сохранение обновленного списка заметок
+                SaveNotes();
+                // Вывод сообщения об успешном сохранении заметки
+                MessageBox.Show("Заметка сохранена", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Вывод сообщения об ошибке в случае отсутствия текста в заметке
+                MessageBox.Show("Введите текст заметки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Обработчик события выбора даты в календаре
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            // Получение выбранной даты
+            DateTime selectedDate = monthCalendar1.SelectionStart;
+            // Поиск заметки по выбранной дате
+            Note note = FindNoteByDate(selectedDate);
+
+            // Если заметка найдена
+            if (note != null)
+            {
+                // Отображение текста заметки
+                textBox1.Text = note.Text;
+                // Изменение текста кнопки на "Изменить"
+                button2.Text = "Изменить";
+                // Включение кнопки удаления заметки
+                button1.Enabled = true;
+            }
+            else
+            {
+                // Очистка текстового поля
+                textBox1.Text = "";
+                // Изменение текста кнопки на "Сохранить"
+                button2.Text = "Сохранить";
+                // Отключение кнопки удаления заметки
+                button1.Enabled = false;
+            }
+        }
+
+        // Метод для загрузки заметок из файла
+        private void LoadNotes()
+        {
+            // Проверка существования файла с заметками
+            if (File.Exists(filePath))
+            {
+                // Чтение JSON из файла
+                string json = File.ReadAllText(filePath);
+                // Десериализация JSON в список заметок
+                notes = JsonConvert.DeserializeObject<List<Note>>(json);
+            }
+        }
+
+        // Метод для сохранения заметок в файл
+        private void SaveNotes()
+        {
+            // Сериализация списка заметок в JSON
+            string json = JsonConvert.SerializeObject(notes);
+            // Запись JSON в файл
+            File.WriteAllText(filePath, json);
+        }
+
+        // Метод для поиска заметки по дате
+        private Note FindNoteByDate(DateTime date)
+        {
+            // Поиск заметки в списке по заданной дате
+            return notes.Find(note => note.Date.Date == date.Date);
+        }
+    }
+
+    // Класс для представления заметки
+    public class Note
+    {
+        // Дата заметки
+        public DateTime Date { get; set; }
+        // Текст заметки
+        public string Text { get; set; }
+    }
+}
